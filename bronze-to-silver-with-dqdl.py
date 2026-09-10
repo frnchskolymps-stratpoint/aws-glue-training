cd62e0a8437099191f2af6cfc49169789efa33db
francheska.olympia <francheska.olympia@EDP-100-1289-CheskaOlympia.local>
2026-09-10 13:41:01 +0800

    added script of bronze-to-silver etl

---
+++ bronze-to-silver-with-dqdl.py
@@ -0,0 +1,118 @@
+import sys
+import boto3
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.sql.functions import col
+from awsglue.dynamicframe import DynamicFrame
+from awsgluedq.transforms import EvaluateDataQuality
+
+def read_datacatalog(glueContext, db_name, table_name):
+    try:
+        return glueContext.create_dynamic_frame.from_catalog(
+            database = db_name,
+            table_name = table_name
+            )
+    except Exception as e:
+        print(f"Error reading table {table_name}: {e}")
+        raise e
+
+# fetching the DQDL ruleset from the Data Catalog using boto3
+def get_catalog_ruleset(ruleset_name):
+    try:
+        glue_client = boto3.client('glue')
+        response = glue_client.get_data_quality_ruleset(Name=ruleset_name)
+        return response['Ruleset']  # Returns raw DQDL string
+    except Exception as e:
+        print(f"Error retrieving ruleset '{ruleset_name}' from Data Catalog: {e}")
+        raise e
+        
+def evaluate_and_split_dqdl(glueContext, dynamic_frame, ruleset_name):
+    try:
+        dqdl_string = get_catalog_ruleset(ruleset_name)
+        
+        # Execute DQ processing
+        dq_results = EvaluateDataQuality().process_rows(
+            frame=dynamic_frame,
+            ruleset=dqdl_string,
+            publishing_options={
+                "dataQualityEvaluationContext": ruleset_name,
+                "enableDataQualityCloudWatchMetrics": True,
+                "enableDataQualityResultsPublishing": True
+            },
+            additional_options={
+                "performanceTuning.caching": "CACHE_NOTHING",
+                "observations.scope": "ALL"
+            }
+        )
+        
+        # Extract 'rowLevelOutcomes' which holds the original rows + DataQualityEvaluationResult
+        df = dq_results["rowLevelOutcomes"].toDF()
+        
+        # Safe column dropping list
+        dq_cols = ["DataQualityEvaluationResult", "DataQualityRulesPass", "DataQualityRulesFail", "DataQualityRulesSkip"]
+        cols_to_drop = [c for c in dq_cols if c in df.columns]
+        
+        passed_df = df.filter(df["DataQualityEvaluationResult"] == "PASSED").drop(*cols_to_drop)
+        quarantine_df = df.filter(df["DataQualityEvaluationResult"] == "FAILED").drop(*cols_to_drop)
+        
+        return passed_df, quarantine_df
+    except Exception as e:
+        print(f"Error processing DQDL evaluation for {ruleset_name}: {e}")
+        raise e
+
+        
+def write_to_silver_s3 (df, silver_path):
+    try:
+        df.write.mode("append").format("parquet").save(silver_path)
+    except Exception as e:
+        print(f"Error saving df to {silver_path}. {e}")
+        raise e
+    
+def main():
+    args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+    sc = SparkContext()
+    glueContext = GlueContext(sc)
+    spark = glueContext.spark_session
+    job = Job(glueContext)
+    job.init(args['JOB_NAME'], args)
+    
+    try:
+        db_name = "bronze-cheska-glue-training"
+        orders_table = "olist_orders_dataset_csv"
+        payments_table = "olist_order_payments_dataset_csv"
+        silver_passed_path = "s3://cheska-s3-medallion/silver/passed/"
+        silver_quarantine_orders_path = "s3://cheska-s3-medallion/silver/quarantine/orders/"
+        silver_quarantine_payments_path = "s3://cheska-s3-medallion/silver/quarantine/payments/"
+
+        # kept data as dynamicframe at ingestion
+        orders_dyf = read_datacatalog(glueContext, db_name, orders_table)
+        payments_dyf = read_datacatalog(glueContext, db_name, payments_table)
+
+        # called the function of dqdl in execution flow
+        passed_orders_df, quarantine_orders_df = evaluate_and_split_dqdl(
+            glueContext, orders_dyf, "orders_dqdl_ruleset"
+        )
+
+        passed_payments_df, quarantine_payments_df = evaluate_and_split_dqdl(
+            glueContext, payments_dyf, "payments_dqdl_ruleset"
+        )
+        
+        write_to_silver_s3(quarantine_orders_df, silver_quarantine_orders_path)
+        write_to_silver_s3(quarantine_payments_df, silver_quarantine_payments_path)
+        
+        join_pass_df = passed_orders_df.join(passed_payments_df, on="order_id", how="inner")
+        
+        write_to_silver_s3(join_pass_df, silver_passed_path)
+        
+        job.commit()
+        print(f"job completed successfully")
+        
+    except Exception as e:
+        print(f"job failed: {e}")
+        raise e
+        
+if __name__ == "__main__":
+    main()
